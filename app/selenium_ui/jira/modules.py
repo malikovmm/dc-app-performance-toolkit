efee35ef8b17b9e2d7ac6203b98728a15d0ad5dd
maksim <maksim.malikov@smartforce.io>
2022-03-22 15:13:23 +0600

    add debug

--- app/selenium_ui/jira/modules.py
+++ app/selenium_ui/jira/modules.py
@@ -30,8 +30,6 @@ def setup_run_data(datasets):
     datasets['project_key'] = projects[0]
     if SW_PAGES in datasets:
         if len(datasets[SW_PAGES]) > 0:
-            with open('file.txt', 'w') as file:
-                 file.write(json.dumps(sw_page))
             sw_page = random.choice(datasets[SW_PAGES])
             datasets['sw_project_key'] = sw_page[2]
             datasets['sw_page_key'] = sw_page[1]
